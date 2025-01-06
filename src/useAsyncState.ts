db29e20b2bccaf7cff1369ff74f4f75ef5487d6e
zsh2401 <zsh2401@163.com>
2025-01-07 01:32:11 +0800

    完善了一系列问题

--- src/useAsyncState.ts
+++ src/useAsyncState.ts
@@ -1,25 +1,56 @@
 import { useState, useEffect, useCallback, DependencyList } from "react"
+import lf from "localforage"
+/**
+ * Async state status
+ */
 export enum Status {
     "pending",
     "fulfilled",
     "rejected"
 }
+/**
+ * Remote data loader.
+ */
 export interface AsyncStateLoader<A, S> {
     (args?: Partial<A>): Promise<S> | S;
 }
+/**
+ * Trigger reload without dependencies' update
+ */
 export interface Reloader<A, S> {
     (args?: Partial<A>): Promise<S>;
 }
+/**
+ * Set the state manually
+ */
 export interface StateSetter<S> {
     (newState: S): void;
 }
 export interface UseAsyncStateOptions<A, S> {
+
+    /**
+     * Local cache key.
+     */
+    localCacheKey?: string
+
+    /**
+     * Initial state when loader's promise is not resolved.
+     */
     initialState: S;
+
+    /**
+     * Async data loader such as fetch operation, ajax and etc.
+     */
     loader: AsyncStateLoader<A, S>;
+
     /**
      * @default false
      */
     reloadOnMounted?: boolean
+
+    /**
+     * Dependencies
+     */
     deps?: DependencyList
 
     /**
@@ -36,15 +67,20 @@ export interface ReturnValue<A, S> {
     reload: Reloader<A, S>;
     setter: StateSetter<S>;
 }
+/**
+ * Better way to manage your asynchronous state.
+ * @author zsh2401
+ * @param options 
+ * @returns 
+ */
 export function useAsyncState<A, S>(options: UseAsyncStateOptions<A, S>):
     ReturnValue<A, S> {
 
     const [state, stateSetter] = useState<S>(options.initialState);
-
     const [status, statusSetter] = useState<Status>(Status.pending);
 
-    const rawCachedFn = useCallback(options.loader, [...(options.deps ?? [])])
-    const fn = useCallback(async (args?: Partial<A>) => {
+    const rawCachedFn = useCallback(options.loader,options.deps ?? [])
+    const asyncStateOperator = useCallback(async (args?: Partial<A>) => {
         try {
             const data = await rawCachedFn(args);
             stateSetter(data);
@@ -55,28 +91,40 @@ export function useAsyncState<A, S>(options: UseAsyncStateOptions<A, S>):
             statusSetter(Status.rejected);
             throw err
         }
-    }, [...(options.deps ?? []), rawCachedFn]);
+    }, [rawCachedFn, options.onError, statusSetter, stateSetter]);
 
     const [firstTime, setFirstTime] = useState(true)
 
     useEffect(() => {
-        if (options.reloadOnMounted && firstTime) {
-            fn()
-            setFirstTime(false)
-        } else if (!firstTime) {
-            fn()
-            setFirstTime(false)
-        }
-    }, [fn, firstTime])
+        (async () => {
 
-    // useEffect(() => {
-    //     fn()
-    // }, []);
+            if (firstTime && options.localCacheKey) {
+                // Local cache key has been set, try load it first
+                const cachedValue = await lf.getItem<S>(options.localCacheKey)
+                if (cachedValue !== null) {
+                    // Fulfilled
+                    statusSetter(Status.fulfilled)
+                    stateSetter(cachedValue)
+                }
+            }
+
+            const shouldLoadWhenMounted = firstTime && options.reloadOnMounted;
+            const isDepUpdated = !firstTime
+
+            if (shouldLoadWhenMounted || isDepUpdated) {
+                const newState = await asyncStateOperator()
+                if (options.localCacheKey) {
+                    await lf.setItem(options.localCacheKey, newState)
+                }
+            }
+            setFirstTime(false)
+        })()
+    }, [asyncStateOperator, options.reloadOnMounted, options.localCacheKey, firstTime])
 
     return {
         state,
         loadingStatus: status,
-        reload: fn,
+        reload: asyncStateOperator,
         setter: stateSetter
     };
 }
